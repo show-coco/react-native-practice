aa64ed95c914a82c24b1ed0c8790d1dfba6cfbf3
Sho Sakai <show75@ezweb.ne.jp>
2021-01-22 10:56:28 +0900

    chore: Setup lint

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, TextInputBase, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <TextInput style={{
+      <TextInput
+        style={{
           height: 40,
           width: 200,
-          borderColor: 'gray',
-          borderWidth: 1
+          borderColor: "gray",
+          borderWidth: 1,
         }}
       />
       <Button
-  onPress={() => {
-    alert('You tapped the button!');
-  }}
-  title="Press Me"
-/>
+        onPress={() => {
+          alert("You tapped the button!");
+        }}
+        title="Press Me"
+      />
 
       <StatusBar style="auto" />
     </View>
@@ -28,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
