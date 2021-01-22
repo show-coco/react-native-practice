d91a7dad1f9e564dc6f88555e77025a9400fc33f
Sho Sakai <show75@ezweb.ne.jp>
2021-01-22 11:22:48 +0900

    Learn View

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,4 @@ module.exports = {
     "prettier",
     "prettier/@typescript-eslint",
   ],
-  rules: {
-    "no-alert": false,
-  },
 };
--- App.tsx
+++ App.tsx
@@ -1,34 +1,30 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  const onPress = () => {
-    alert("You tapped the button!");
-  };
-
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <TextInput style={styles.textInput} />
-      <Button onPress={onPress} title="Press Me" />
-
-      <StatusBar style="auto" />
+    <View
+      style={styles.container}
+      accessibilityHint="You can see blue and red box"
+    >
+      <View style={styles.blueChild} />
+      <View style={styles.redChild} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
+    height: 100,
+    padding: 20,
   },
-  textInput: {
-    height: 40,
-    width: 200,
-    borderColor: "gray",
-    borderWidth: 1,
+  blueChild: {
+    backgroundColor: "blue",
+    flex: 0.3,
+  },
+  redChild: {
+    backgroundColor: "red",
+    flex: 0.5,
   },
 });
